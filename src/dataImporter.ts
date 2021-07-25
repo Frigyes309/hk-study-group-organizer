e524876459430dda91a7083d2587b0343b14724e
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-07-25 17:45:22 +0200

    Create excel import functions

--- src/dataImporter.ts
+++ src/dataImporter.ts
@@ -1,46 +1,171 @@
+import path from "path";
+
 const excel = require('convert-excel-to-json')
 import { Students } from "./Students";
 import chalk from "chalk";
 
-export class dataImporter{
-  /**
-   * @description
-   * Imports excel table containing Students
-   * @param path Path to the current excel file
-   * @param columnMap column ids ( eg. A, B, ... ) mapped to object names ( eg. neptun, roomSenior, ... )
-   * @param rowsToSkip how many first rows to skip, default: 1
-   * @param sheetName name of the sheet which to read from, if only one sheet is present it's not necessary
-   */
-  public static import(path: string, columnMap: { [key: string]: string }, sheetName: string | undefined, rowsToSkip: 1){
-    const data = excel({
-      sourceFile: path,
-      header: {
-        rows: rowsToSkip
-      },
-      columnToKey: columnMap
-    });
+/**
+ * @description
+ * Imports a general sheet from any excel file based on the columnMap provided
+ * @param path Path to the current excel file
+ * @param columnMap column ids ( eg. A, B, ... ) mapped to object names ( eg. neptun, roomSenior, ... )
+ * @param rowsToSkip how many first rows to skip, default: 1
+ * @param sheetName name of the sheet which to read from, if only one sheet is present it's not necessary
+ *
+ * @returns excelSheet|undefined Excel sheet converted to an array of objects, object keys from columnMap
+ */
+function importSheet(path: string, columnMap: { [key: string]: string }, sheetName: string|undefined = undefined, rowsToSkip = 1) : [{[key: string]: any}]|undefined{
+  const data = excel({
+    sourceFile: path,
+    header: {
+      rows: rowsToSkip
+    },
+    columnToKey: columnMap
+  });
 
-    if(!data){
-      console.error(chalk.red('[Excel Import]: ') + "Can' t read excel file: " + path);
-    }
-
-    //Get the sheet from excel, first try to get the sheet by name
-    //If no name is provided and only one sheet exists grab the first sheet
-    let sheet;
-    if(sheetName){
-      sheet = data[sheetName];
-      if(!sheet){
-        console.error(chalk.red("[Excel Import]: ") + `Can't find this sheet: [${sheetName}]`);
-      }
-    }else{
-      let keys = Object.keys(data);
-      if(keys.length > 1){
-        console.error(chalk.red("[Excel Import]: ") + `No sheet name is provided for import and there are multiple sheets: [${keys.join()}]`);
-      }
-      //In excel at least one sheet is always present
-      sheet = data[keys[0]];
-    }
-
-
+  if(!data){
+    console.error(chalk.red('[Excel Import]: ') + "Can' t read excel file: " + path);
   }
+
+  //Get the sheet from excel, first try to get the sheet by name
+  //If no name is provided and only one sheet exists grab the first sheet
+  let sheet;
+  const keys = Object.keys(data);
+  if(sheetName){
+    sheet = data[sheetName];
+    if(!sheet){
+      console.error(chalk.red("[Excel Import]: ") + `Can't find this sheet: [${sheetName}], available names: [${keys.join()}]`);
+    }
+  }else{
+    if(keys.length > 1){
+      console.error(chalk.red("[Excel Import]: ") + `No sheet name is provided for import and there are multiple sheets: [${keys.join()}]`);
+    }
+    //In excel at least one sheet is always present
+    sheet = data[keys[0]];
+  }
+  return sheet;
+}
+
+/**
+ * Imports an excel file containing students who were in GTB
+ * @param path Path to the excel file
+ * @param sheetName Name of the sheet if more than one is present in the excel
+ *
+ */
+export function importGTB(path: string, sheetName: string|undefined = undefined) : [StudentGTB] | undefined{
+  let sheet = importSheet(path, {
+    A: 'neptun',
+    B: 'roomSenior',
+    C: 'cardSenior',
+    D: 'color'
+  }, sheetName);
+  if(!sheet){
+    console.log(chalk.red('[Excel Import]: ') + `Can't import GTB excel file: ${path}`);
+    return undefined;
+  }
+  //Convert the neptun code to uppercase format
+  sheet.map(row => {
+    return {
+      ...row,
+      neptun: row.neptun.toUpperCase()
+    }
+  });
+  // @ts-ignore
+  return sheet;
+}
+
+/**
+ * Imports an excel file containing students who will live in the dormitory
+ * @param path Path to the excel file
+ * @param sheetName Name of the sheet if more than one is present in the excel
+ */
+export function importDormitory(path: string, sheetName: string|undefined = undefined) : [StudentDormitory] | undefined{
+  let sheet = importSheet(path, {
+    A : 'name',
+    D : 'neptun',
+    G : 'score',
+    I : 'major',
+    K : 'admissionType',
+    L : 'color',
+    M : 'room',
+  }, sheetName);
+  if(!sheet){
+    console.log(chalk.red('[Excel Import]: ') + `Can't import Dormitory excel file: ${path}`);
+    return  undefined;
+  }
+  //Convert the neptun code to uppercase format
+  sheet.map(row => {
+    return {
+      ...row,
+      neptun: row.neptun.toUpperCase()
+    }
+  });
+  // @ts-ignore
+  return sheet;
+}
+
+/**
+ * Imports an excel file containing all students who got accepted to the University
+ * @param path Path to the excel file
+ * @param sheetName Name of the sheet if more than one is present in the excel
+ */
+export function importDH(path: string, sheetName: string|undefined = undefined) : [StudentDH] | undefined{
+  let sheet = importSheet(path, {
+    A : 'neptun',
+    B : 'name',
+    C : 'zipCode',
+    D : 'gender',
+    E : 'imsc',
+    F : 'doublePassive',
+    G : 'german',
+  }, sheetName);
+  if(!sheet){
+    console.log(chalk.red('[Excel Import]: ') + `Can't import DH excel file: ${path}`);
+    return  undefined;
+  }
+  //Convert the neptun code to uppercase format
+  // @ts-ignore
+  sheet = sheet.map(row => {
+    return {
+      ...row,
+      neptun: row.neptun.toUpperCase(),
+      gender: row.gender == 'Férfi' ? 'F' : 'N',
+      imsc: row.imsc == 'X',
+      doublePassive: row.doublePassive == 'X',
+      german: row.german == 'X',
+    }
+  });
+  // @ts-ignore
+  return sheet;
+}
+
+/**
+ * Excel imported dataTypes
+ */
+type StudentGTB = {
+  neptun : string,
+  roomSenior : string,
+  cardSenior: string,
+  color : string,
+}
+
+type StudentDormitory = {
+  name : string,
+  neptun : string,
+  score: number,
+  major: string,
+  admissionType: string,
+  color: string,
+  room: number,
+}
+
+type StudentDH = {
+  neptun: string,
+  name: string,
+  zipCode: number,
+  /** Gender of this student, stored in a hungarian format (F - Male) (N - Female) */
+  gender: 'F' | 'N',
+  imsc: boolean,
+  doublePassive: boolean,
+  german: boolean
 }
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,19 @@
-const excelToJson = require('convert-excel-to-json');
-import { groupBy } from "./utils";
+import { importDH, importDormitory } from "./dataImporter";
 import express from 'express';
 import path from 'path';
 
-const baseDataDir = '/Users/balint/Documents/GitHub/hk-study-group-organizer/data/';
+const dataDir = '/Users/balint/Documents/GitHub/hk-study-group-organizer/data/';
 const app = express();
 // sets ejs views folder
 app.set('views', path.join(__dirname, '../views'));
 // sets view engine
 app.set('view engine', 'ejs');
 
-const gtbData = excelToJson({
-  sourceFile: baseDataDir + 'golyatabor.xlsx',
-  header: {
-    rows: 1
-  },
-  columnToKey: {
-    A: 'neptun',
-    B: 'roomSenior',
-    C: 'cardSenior',
-    D: 'color'
-  }
-});
-
-type gtbStudent = {
-  neptun: string,
-  roomSenior: string,
-  cardSenior: string,
-  color: string
-}
-
-const schData = excelToJson({
-  sourceFile: baseDataDir + 'koli.xlsx',
-  header: {
-    rows: 1
-  },
-  columnToKey: {
-    A: 'name',
-    B: 'neptun',
-    C: 'major',
-    D: 'color',
-    E: 'card',
-    F: 'gender',
-    G: 'room'
-  }
-});
-
-type Student = {
-  name: string,
-  neptun: string,
-  major: string,
-  color: string,
-  card: string,
-  gender: string,
-  room: string
-};
-
-type vector = {
-  room: number,
-  gtb: number,
-  color: string
-};
+let a = importDH(path.join(dataDir, 'VIK-alapképzés-felvettek-2020A-besoroláshoz.xlsx'), 'Info');
+console.log(a!.filter((b) => {
+  return b.german ? b : undefined
+}).length);
+process.exit();
 
 let colors = new Map<string, {color: string, floor: number, gtb: number}>([
   ['DrWu', {color: 'yellow', floor: 600, gtb: 0}],
@@ -69,59 +22,6 @@ let colors = new Map<string, {color: string, floor: number, gtb: number}>([
   ['SIR', {color: 'gray', floor: 500, gtb: 120}],
   ['TTNY', {color: 'red', floor: 900, gtb: 160}]
 ]);
-
-let studentVectorMap = new Map<string, vector>(); //Neptun - vector
-
-schData.Munka1.forEach((student: Student) => {
-  student.neptun = student.neptun.toUpperCase();
-  let vec = studentVectorMap.get(student.neptun) || {room: -1, gtb: -1, color: 'gray'};
-
-  if(student.room){
-    let room = Array.from(student.room.toString());
-    //vec.room = Number(room.shift())*100 + Number(room.join(''))*10;
-    vec.room = Number(student.room);
-  }
-
-  if(!student.color) return;
-  vec.color = colors.get(student.color)!.color;
-  studentVectorMap.set(student.neptun, vec);
-});
-
-let cardSeniors = new Map<string, string>();
-let roomSeniors = new Map<string, string>();
-
-Array.from(groupBy(gtbData['Beosztás'], (x: gtbStudent) => x.cardSenior)).map(([card, students]) => {
-  return {
-    card,
-    room: Array.from(groupBy(students, y => y.roomSenior).keys())
-  }
-}).forEach((value, index) => {
-  cardSeniors.set(value.card, index.toString());
-  value.room.forEach((val, ix) => {
-    roomSeniors.set(val, ix.toString());
-  })
-})
-
-gtbData['Beosztás'].forEach((student: gtbStudent) => {
-  student.neptun = student.neptun.toUpperCase();
-  let vec = studentVectorMap.get(student.neptun) || {room: -1, gtb: -1, color: 'gray'};
-  vec.gtb = Number(cardSeniors.get(student.cardSenior)! + Number(roomSeniors.get(student.roomSenior)!));
-  vec.color = colors.get(student.color)!.color;
-  studentVectorMap.set(student.neptun, vec);
-});
-
-const returnData = Array.from(studentVectorMap).map(([neptun, vec]) => {
-  let color = gtbData['Beosztás'].find((x: gtbStudent) => x.neptun.toUpperCase() === neptun.toUpperCase());
-  if(!color) color = schData.Munka1.find((x: Student) => x.neptun.toUpperCase() === neptun.toUpperCase());
-  if(!color) return [neptun, vec];
-  //TODO: Get dorm max room number from the input data
-  if(vec.room === -1) vec.room = colors.get(color.color)!.floor + Math.floor(Math.random() * 15);
-  //TODO: Get gtb max-min from input data
-  if(vec.gtb === -1) vec.gtb = colors.get(color.color)!.gtb + Math.floor(Math.random() * 32);
-
-  return [neptun, vec];
-});
-console.log(returnData);
 
 app.listen(3000, () => {
   console.log(`Listening on port: ${3000}`);
@@ -132,5 +32,5 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/data', (req, res) => {
-  res.json(returnData);
+  res.json([]);
 })
