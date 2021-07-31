ac74f1810d2a786f1e5841a4181dc556faaa2720
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-07-31 17:39:29 +0200

    wip

--- src/studentsImporter.ts
+++ src/studentsImporter.ts
@@ -1,54 +1,62 @@
-import { importDH, importDormitory, importGTB } from "./dataImporter";
-import { Students } from "./Students";
+import { importDH, importDormitory, importGTB } from './dataImporter';
+import { Students } from './Students';
 
 /**
  * @description Imports a group [Vill, Infó, Üzinfó] of students
  * @param filePaths Object containing the excel file paths [DH, Dormitory, GTB]
  * @param major Which major to import [Vill, Infó, Üzinfó]
  */
-export function importStudents(filePaths: { DH: string, Dorm: string, GTB: string }, major: 'Vill' | 'Infó' | 'Üzinfó'){
-  //TODO: Before import new data clear the Students class container
+export function importStudents(
+    filePaths: { DH: string; Dorm: string; GTB: string },
+    major: 'Vill' | 'Infó' | 'Üzinfó',
+) {
+    //TODO: Before import new data clear the Students class container
 
-  const infoDH = importDH(filePaths.DH, major); //TODO: This major -> excel sheet mapping may needs to be changed
-  if(!infoDH){
-    //TODO: Create better error handling
-    process.exit(-1);
-  }
-  const infoDorm = importDormitory(filePaths.Dorm, 'Bsc elsőéves felvettek');
-  if(!infoDorm){
-    //TODO: Create better error handling
-    process.exit(-1);
-  }
-  const infoGTB = importGTB(filePaths.GTB);
-  if(!infoGTB){
-    //TODO: Create better error handling
-    process.exit(-1);
-  }
+    const infoDH = importDH(filePaths.DH, major); //TODO: This major -> excel sheet mapping may needs to be changed
+    if (!infoDH) {
+        //TODO: Create better error handling
+        process.exit(-1);
+    }
+    const infoDorm = importDormitory(filePaths.Dorm, 'Bsc elsőéves felvettek');
+    if (!infoDorm) {
+        //TODO: Create better error handling
+        process.exit(-1);
+    }
+    const infoGTB = importGTB(filePaths.GTB);
+    if (!infoGTB) {
+        //TODO: Create better error handling
+        process.exit(-1);
+    }
 
-  let huColor = new Map<string, string>([
-    ['sárga', 'yellow'],
-    ['fekete', 'black'],
-    ['kék', 'blue'],
-    ['fehér', 'white'],
-    ['piros', 'red'],
-  ])
+    let huColor = new Map<string, string>([
+        ['sárga', 'yellow'],
+        ['fekete', 'black'],
+        ['kék', 'blue'],
+        ['fehér', 'white'],
+        ['piros', 'red'],
+    ]);
 
-  infoDH.forEach((student) => {
+    infoDH.forEach((student) => {
+        const studentGTB = infoGTB.find((s) => s.neptun === student.neptun);
+        const studentDorm = infoDorm.find((s) => s.neptun === student.neptun);
 
-    const studentGTB = infoGTB.find((s) => s.neptun === student.neptun);
-    const studentDorm = infoDorm.find((s) => s.neptun === student.neptun);
+        const color = studentGTB
+            ? huColor.get(studentGTB.color)
+            : studentDorm
+            ? huColor.get(studentDorm.color)
+            : 'gray';
 
-    const color = studentGTB ? huColor.get(studentGTB.color) : (studentDorm ? huColor.get(studentDorm.color) : 'gray');
+        //TODO: Filter out IMSC, and German student groups
 
-    Students.instance.add({
-      ...student,
-      roomSenior: studentGTB ? studentGTB.roomSenior : '' ,
-      cardSenior: studentGTB ? studentGTB.cardSenior : '',
-      color: color ? color : 'gray',
-      admissionType: studentDorm ?  studentDorm.admissionType : '',
-      major: studentDorm ? studentDorm.major : '',
-      room: studentDorm ? studentDorm.room : 0,
-      score: studentDorm ? studentDorm.score : 0,
+        Students.instance.add({
+            ...student,
+            roomSenior: studentGTB ? studentGTB.roomSenior : '',
+            cardSenior: studentGTB ? studentGTB.cardSenior : '',
+            color: color ? color : 'gray',
+            admissionType: studentDorm ? studentDorm.admissionType : '',
+            major: studentDorm ? studentDorm.major : '',
+            room: studentDorm ? studentDorm.room : 0,
+            score: studentDorm ? studentDorm.score : 0,
+        });
     });
-  });
 }
